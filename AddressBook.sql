a646efe18bbbb8a37a38fc5ddadb1ee1cdb23783
senpooja <poojasen744055@gmail.com>
2022-08-26 17:54:26 +0530

    Insert data into table

--- AddressBook.sql
+++ AddressBook.sql
@@ -12,3 +12,9 @@ PhoneNumber BigInt,
 Email varchar(200)
 )
 select * from Address_Book_Table
+
+------ UC 3: Insert Values to Table ------
+Insert into Address_Book_Table(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
+values('Ankita','Raghuwanshi','Tade colony','Indore','MP',243001,9454545454,'bittan@gmail.com'),
+('Ruju','Mathialagan','836 Heritage Rd','Chennai','Tamil Nadu',134002,98402000,'Ruju@gmail.com'),
+('Anita','Yadav','19 Augusta Avenue','Lucknow','Uttar Pradesh',113201,87210505053,'anita@gmail.com');
